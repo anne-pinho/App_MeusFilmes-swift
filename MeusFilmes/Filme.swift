58abef8cac3ffea4af9abb6599d183488ae662e3
anne-pinho <anne.pinhov@gmail.com>
2021-12-15 11:18:05 -0400

    adicionando listagem de filmes

---
+++ MeusFilmes/Filme.swift
@@ -0,0 +1,21 @@
+//
+//  Filme.swift
+//  MeusFilmes
+//
+//  Created by Carol Pinho on 15/12/21.
+//
+
+import UIKit
+
+class Filme {
+    var title: String!
+    var description: String!
+    var image: UIImage!
+
+    
+    init(title: String, description: String){
+        self.title = title
+        self.description = description
+    }
+}
+
--- MeusFilmes/ViewController.swift
+++ MeusFilmes/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var filmes: [Filme] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var filme: Filme
+        
+        filme = Filme(title: "filme 1", description: "descricao 1")
+        filmes.append(filme)
+        
+        filme = Filme(title: "filme 2", description: "descricao 2")
+        filmes.append(filme)
     }
-
-
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filmes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let filme: Filme = filmes [indexPath.row]
+        let celulaReuso = "celulaReuso"
+        
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celulaReuso", for: indexPath)
+        var conteudo = celula.defaultContentConfiguration()
+        conteudo.text = filme.title
+        celula.contentConfiguration = conteudo
+        
+        return celula
+    }
 }
 
